3d649c367f9ab5f97a25cfe6c62d0ba010d7da5a
umer7254 <136007876+umer7254@users.noreply.github.com>
2023-07-31 10:57:32 +0500

    button file

---
+++ button.js
@@ -0,0 +1,66 @@
+
+let  cont=""
+let contEL=document.getElementById('cont')
+
+function one(){
+
+   contEL.innerHTML+=1
+    
+}
+let
+function two(){
+
+    contEL.innerHTML+=2
+    
+}
+function three(){
+
+    contEL.innerHTML+=3
+    
+}
+function addition(){
+    
+    contEL.innerHTML+="+"
+}
+function four(){
+
+    contEL.innerHTML+=4
+    
+}
+function five(){
+    contEL.innerHTML+=5
+    
+}
+function six(){
+    contEL.innerHTML+=6
+}
+function subtraction(){
+    contEL.innerHTML+="-"   
+}
+function siven(){
+    contEL.innerHTML+= 7   
+}
+function eight(){
+    contEL.innerHTML+= 8   
+}
+function nine(){
+    contEL.innerHTML+= 9
+}
+function multiplication(){
+    contEL.innerHTML+="x"
+    
+}
+function ziro(){
+    contEL.innerHTML+= 0
+}
+function dout(){
+    contEL.innerHTML+= "."
+}
+function dwiding(){
+    contEL.innerHTML+= "/"
+}
+function equle(){
+    let result = eval(cont) 
+    contEL.innerHTML= result
+}
+
